411cedade743419817e6854a8858c69870549201
tina.g <77801352+cristinabadea@users.noreply.github.com>
2023-02-27 22:49:16 +0000

    Added the main code

--- index.js
+++ index.js
@@ -11,5 +11,192 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const { default: generate } = require("@babel/generator");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const employees = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the Manager's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the Manager's id?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the Manager's email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the Manager's office number?",
+      name: "officeNumber",
+    },
+  ])
+  .then((response) => {
+    const newManager = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.officeNumber
+    );
+    // add the new Manager object to your array of employees
+    employees.push(newManager);
+
+    promptForNextEmployee();
+  });
+
+const promptForNextEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "chooseEmployee",
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Manager",
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members",
+        ],
+      },
+    ])
+    .then((response) => {
+      if (response.chooseEmployee == "Engineer") {
+        promptForEngineer();
+      } else if (response.chooseEmployee == "Intern") {
+        promptForIntern();
+      } else if (response.chooseEmployee == "Manager") {
+        promptForManager();
+      } else {
+        buildPage();
+      }
+    });
+};
+
+const promptForEngineer = () => {
+  inquirer
+    .prompt([
+      //engineer questions
+      {
+        type: "input",
+        message: "What is the Engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the Engineer's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the Engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the Engineer's GutHub username?",
+        name: "github",
+      },
+    ])
+    .then((response) => {
+      // create a new Engineer object using the user's responses
+      const newEngineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      // add the new Engineer object to your array of employees
+      employees.push(newEngineer);
+      // call the function to prompt the user for the next employee
+      promptForNextEmployee();
+    });
+};
+
+const promptForIntern = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the Intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the Intern's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the Intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the Intern's current school?",
+        name: "school",
+      },
+    ])
+    .then((response) => {
+      const newIntern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      // add the new Intern object to your array of employees
+      employees.push(newIntern);
+      // call the function to prompt the user for the next employee
+      promptForNextEmployee();
+    });
+};
+
+const promptForManager = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the Manager's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the Manager's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the Manager's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the Manager's office number?",
+        name: "officeNumber",
+      },
+    ])
+    .then((response) => {
+      const newManager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
+      // add the new Manager object to your array of employees
+      employees.push(newManager);
+      // call the function to prompt the user for the next employee
+      promptForNextEmployee();
+    });
+};
+
+const buildPage = () => {
+  fs.appendFile("newHTML.html", render(employees), (err) => {
+    err ? console.error(err) : console.log("Check the new HTML file!");
+  });
+};
